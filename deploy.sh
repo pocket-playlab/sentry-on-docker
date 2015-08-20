e1666a519b673693c0e9b3d07053824922dcf356
Sebastian Sasu <sebastian.s@pocketplaylab.com>
2015-08-20 11:09:34 +0700

    Removed garbage code and added production to circle.yml

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-set -e -u
-
-pull() {
-  local server=$1
-  echo "Pulling repository on $server"
-  ssh core@$server "docker pull pocketplaylab/$CIRCLE_PROJECT_REPONAME:$CIRCLE_BRANCH"
-}
-
-# Need to pull on all servers
-for i in $(env|grep CORE|cut -d "=" -f 2)
-do
-  pull "$i" &
-done
-wait
-
-# Then connect to one controller is enough
-ssh core@$CORE1  "fleetctl stop sentry presence-sentry && fleetctl start sentry presence-sentry"
-########
-#!/bin/bash
-
 # set -u fails the script if there are any undefiend environment variables
 
 set -e -u
