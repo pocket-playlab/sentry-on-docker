3ec05c4a1137892e5339c57cb9bb2f4729dd13ef
Sebastian Sasu <sebastian.s@pocketplaylab.com>
2015-08-20 09:40:57 +0700

    Added var $CIRCLE_BRANCH and commented the production deployment

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-set -e
+set -e -u
 
 pull() {
   local server=$1
   echo "Pulling repository on $server"
-  ssh core@$server "docker pull pocketplaylab/$CIRCLE_PROJECT_REPONAME:latest"
+  ssh core@$server "docker pull pocketplaylab/$CIRCLE_PROJECT_REPONAME:$CIRCLE_BRANCH"
 }
 
 # Need to pull on all servers
@@ -17,3 +17,53 @@ wait
 
 # Then connect to one controller is enough
 ssh core@$CORE1  "fleetctl stop sentry presence-sentry && fleetctl start sentry presence-sentry"
+########
+#!/bin/bash
+
+# set -u fails the script if there are any undefiend environment variables
+
+set -e -u
+
+pull() {
+  local server=$1
+  echo "Pulling repository on $server"
+  ssh core@$server "docker pull pocketplaylab/$CIRCLE_PROJECT_REPONAME:$CIRCLE_BRANCH"
+}
+
+env=$1
+## Deploy on prod or staging
+## Variables below have to be declared in CircleCI
+if [[ $env == 'production' ]]; then
+  FILTER="DEPLOY_CORE_PROD"
+  MASTER=$DEPLOY_CORE_PROD1
+elif [[ $env == 'staging' ]]; then
+  FILTER="DEPLOY_CORE_STAGING"
+  MASTER=$DEPLOY_CORE_STAGING1
+fi
+###
+# Need to pull on all servers
+for i in $(env|grep $FILTER|cut -d "=" -f 2)
+do
+  pull "$i" &
+done
+wait
+
+# Then connect to one controller is enough
+ssh core@$MASTER  "fleetctl stop sentry presence-sentry \
+	 && fleetctl start sentry presence-sentry"
+
+echo -e "Please wait while the container starts up...\n"
+
+sleep 15
+
+# Grepping <service>.service in case there are other services with similar names
+# Please replace <service> with your own
+echo -e ""
+ssh core@$MASTER "fleetctl list-units |grep sentry"
+echo -e ""
+## Checking status of the unit
+echo -e "Check status ^^^above^^^ about the unit\n"
+echo -e ""
+sleep 15
+echo -e "All done\n"
+echo -e ""
